4cc22b77749c30177400fbcb4edac155005a6e95
Soft Mahmood <nakugobamahmood@gmail.com>
2023-08-31 18:59:47 +0000

    pycodstyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,10 +2,10 @@
 
 """
 Module 0-square
-Defines class square 
+Defines class square
 """
 
-class Square:
-	""" Square class is an empty class that defines a square """
-	pass
 
+class Square:
+    """ Square class is an empty class that defines a square """
+    pass
