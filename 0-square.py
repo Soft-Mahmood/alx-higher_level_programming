dedf9e319aacf51475e58bfa592d655d2e25dc4d
Soft-Mahmood <softmahmood@gmail.com>
2023-09-03 14:23:20 +0300

    5658

--- 0-square.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-class Square:
-	""" Square class is an empty class that defines a square """
-	pass
-
--- 1-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-class Square:
-	""" Square class defines a square by size (private) """
-	def __init__(self, size):
-		""" initializes the size onject """
-		self.__size = size
-
