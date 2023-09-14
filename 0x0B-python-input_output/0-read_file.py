672f862ba32325323df4291f5b28ec00fd19b80d
Soft Mahmood <softmahmood@gmail.com>
2023-09-14 21:27:10 +0000

    documenttion

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """
-this function reads a textfile
+Module 0-read_file
+has function that reads a textfile
 Prints the cotents to stdout
 
 """
 def read_file(filename=""):
-    """ filename is the name of the file """
+    """
+    filename is the name of the file
+    Args:
+        Filename
+    """
     with open(filename, encoding="uft-8") as file
         for line in file:
             print(line, end="")
