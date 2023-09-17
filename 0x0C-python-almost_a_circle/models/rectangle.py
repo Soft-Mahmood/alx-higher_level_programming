d727c1a0ecbb5acbc365da1c856ef6f16d1ec84f
Soft Mahmood <softmahmood@gmail.com>
2023-09-17 13:47:34 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,4 +133,50 @@ class Rectangle(Base):
             raise ValueError('y must be > 0')
         self.__y = value
 
+    def area(self):
+        """
+        Calculates the area of the rect
 
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """ prints the rectangle in stdout with '#' """
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print('#', end="")
+            print()
+
+    def __str__(self):
+        """ string respresentation of the object """
+        return "[Rectangle] ({}) {}/{} -{}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """ updates the instance attributes:
+            1st argument should be the id attribute
+            2nd argument should be the width attribute
+            3rd argument should be the height attribute
+            4th argument should be the x attribute
+            5th argument should be the y attribute
+        """
+        count = 0
+        if args != ():
+            for a in args:
+                count += 1
+                if count == 1:
+                    self.id = a
+                if count == 2:
+                    self.width = a
+                if count == 3:
+                    self.height = a
+                if count == 4:
+                    self.x = a
+                if count == 5:
+                    self.y = a
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
